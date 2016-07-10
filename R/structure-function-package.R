4892fcabd46f1cb0d51aa14d92e638a664cc8dad
Bjørn Kjos-Hanssen <bjoernkjoshanssen@gmail.com>
2016-07-10 10:50:04 -1000

    adding a subdirectory R with a package description

---
+++ R/structure-function-package.R
@@ -0,0 +1,10 @@
+#' Structure Function
+#'
+#' Package description.
+#'
+#' @docType package
+#' @name <structure-function>
+#' @import assertthat
+#' @importFrom utils head tail
+#' @useDynLib <structure-function>
+NULL
